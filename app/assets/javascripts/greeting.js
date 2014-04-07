2748b911efa172cd60e3ed436cfec1611874ebfd
Evan Sitzes <esitzes42@hotmail.com>
2014-04-07 00:53:03 -0500

    added first jquery

---
+++ app/assets/javascripts/greeting.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".right-pic").on("click", function() {
+
+  	var message = $("Call 1-555-jquery-air to book this tour");
+
+  	
+  	$(this).parent().first().append("<p>A Yukata is a traditional Japanese robe to be warn in the summer time.  I was able to go with some Japanese friends and explore Kyomizu-tera in the historical district of Japan's ancient capital, Kyoto.	</br>	日本に留学中、浴衣を着て清水寺に参詣しました。日本の人達が何回も僕と写真を一緒に撮って欲しいと頼んできてくれてとても楽しかったし、良い経験だった！</p>");
+  });
+});
