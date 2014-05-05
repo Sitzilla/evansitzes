b086e5c4a882086e32eaba6d48e15149b419ad2a
Evan Sitzes <esitzes42@hotmail.com>
2014-05-04 20:17:10 -0500

    Added redirect_to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+	def correct_domain
+		  redirect_to "http://www.evansitzes.com/", :status => 301
+	end
+
 end
