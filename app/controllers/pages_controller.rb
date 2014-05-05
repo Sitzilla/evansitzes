c46d70f275d6a29ac6e3a538f95228418b8cd7c4
Evan Sitzes <esitzes42@hotmail.com>
2014-05-04 19:51:36 -0500

    Added detail to meta tags

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
-  		set_meta_tags :description => "Personal blog site for Evan Sitzes built in Ruby on Rails"
+	  		set_meta_tags :description => "'Web development... My story'; evansitzes.com is a personal blog site for Evan Sitzes built in Ruby on Rails. 
+	  																		This site acts as my personal online presence that highlights some of my skills, abilities and interests."
   		set_meta_tags :keywords => "Evan Sitzes - My Story of Web Development" 
   end
 
